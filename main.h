9ae85423f5438a052ea2634d92cbf108fa489008
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-16 12:17:38 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,14 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
-int printf(const char *format, ...);
-int fprintf(FILE *stream, const char *format, ...);
-int dprintf(int fd, const char *format, ...);
-int sprintf(char *str, const char *format, ...);
-int snprintf(char *str, size_t size, const char *format, ...);
-int vprintf(const char *format, va_list ap);
-int vfprintf(FILE *stream, const char *format, va_list ap);
-int vdprintf(int fd, const char *format, va_list ap);
-int vsprintf(char *str, const char *format, va_list ap);
-int vsnprintf(char *str, size_t size, const char *format, va_list ap);
 #endif
