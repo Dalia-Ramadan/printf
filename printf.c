2c06a9f39924643b90fd5afddb305a005bd904a5
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-16 12:22:39 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -9,5 +9,8 @@ int _printf(const char *format, ...)
 	printf ("%c%c\n ", 'a', 96);
 	printf ("%s\n", "dalia");
 	printf ("%%");
+	printf ("%i", 1);
+	printf ("%b", 5);
+	printf ("%o", 10);
 	return (*format);
 }
