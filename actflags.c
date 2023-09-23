95f612db7d69a136e57c5439bcf800b1cfe74dcc
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-22 19:28:12 -0700

    printf

--- actflags.c
+++ actflags.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_flags - Calculates active flags
+ * getflags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int getflags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
--- handling.c
+++ handling.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
 
- * handle_print - Prints an argument based on its type
+ * handleprint - Prints an argument based on its type
 
  * @fmt: Formatted string in which to print the arguments.
 
@@ -24,7 +24,7 @@
 
  */
 
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int handleprint(const char *fmt, int *ind, va_list list, char buffer[],
 
         int flags, int width, int precision, int size)
 
